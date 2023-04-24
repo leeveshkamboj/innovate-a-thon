c80efb744ec003ea4147545b36eb1dd70a30ce72
Leevesh Kamboj <leeveshk12@gmail.com>
2023-04-24 13:43:36 +0530

    firstcommit

---
+++ public/js/countdown-custom.js
@@ -0,0 +1,7 @@
+jQuery(document).ready(function() {
+        $(function () {
+            $('#defaultCountdown').countdown({until: new Date(2023, 4, 10, 0)}); // year, month, date, hour
+        });
+        console.log("test");
+});		
+
