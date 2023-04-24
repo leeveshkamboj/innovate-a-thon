d094a8a6b3b1846fd7bbcd77c7f3810c4a59840f
Sourav Saini <sainisourav279@gmail.com>
2023-04-24 15:11:22 +0530

    thikthak

--- public/js/countdown-custom.js
+++
@@ -1,7 +0,0 @@
-jQuery(document).ready(function() {
-        $(function () {
-            $('#defaultCountdown').countdown({until: new Date(2023, 4, 10, 0)}); // year, month, date, hour
-        });
-        console.log("test");
-});		
-
